93426a157d889284558fdad0bd66aa7bf5c8815b
Niamh <niamh.gilligan@dcblox.com>
2020-02-28 20:26:46 +0000

    feedback changes

--- src/blox_components/Billing/RecentInvoices.js
+++ src/blox_components/Billing/RecentInvoices.js
@@ -9,44 +9,10 @@ const CDN_URL = process.env.REACT_APP_CDN_URL;
 const dueIcon = `${CDN_URL}billing/due-icon.svg`;
 const overdueIcon = `${CDN_URL}billing/icon_Invoice_pastdue.svg`;
 const paidIcon = `${CDN_URL}billing/invoice-icon-paid.svg`;
-
-const EmptyInvoice = {
-	transactions: [
-		{
-			invoiceId: null,
-			customerId: null,
-			invoiceNumber: null,
-			amount: '',
-			outstandingBalance: '',
-			invoiceTotal: '',
-			uri: '',
-			transactionId: null,
-			effectiveDate: '',
-			description: '',
-			transactionType: '',
-			reference: '',
-		},
-	],
-	invoiceNumber: '',
-	billcycle: '',
-	invoiceAmount: '',
-	dueDate: '',
-	amountDue: '',
-	status: null,
-	invoiceId: null,
-	billCycle: undefined,
-	theme: {
-		icon: '',
-		bannerColor: '',
-	},
-	summaryHeaders: {
-		invoiceNumber: 'Invoice',
-		billcycle: 'Sent',
-		invoiceAmount: 'Total',
-		dueDate: 'Due',
-		amountDue: 'Balance',
-	},
-	detailHeaders: { effectiveDate: 'Date', description: 'Description', amount: 'Amount' },
+const NUM_INVOICES_TO_SHOW = {
+	[RESOLUTIONS.LOW]: 2,
+	[RESOLUTIONS.MED]: 3,
+	[RESOLUTIONS.HIGH]: 3,
 };
 
 class RecentInvoices extends Component {
@@ -77,9 +43,7 @@ class RecentInvoices extends Component {
 		const { data: { invoices = [] } = {} } = response;
 
 		if (Utils.isValidResponse(response) && invoices) {
-			this.setState({ invoices }, () => {
-				this.getInvoicesToDisplay();
-			});
+			this.setState({ invoices });
 		} else {
 			this.props.showTitle(false);
 			this.setState({ showRecentPayment: false, error: 'Error pulling recent invoices' });
@@ -107,32 +71,45 @@ class RecentInvoices extends Component {
 		const threeMonths = Sorting.getPrev3Months(new Date());
 		return threeMonths;
 	};
-	getInvoicesToDisplay = () => {
-		try {
-			const { breakpoint } = this.props;
-			const { invoices } = this.state;
-			if (breakpoint === RESOLUTIONS.LOW) {
-				const lastestTwo = BillingUtils.getInvoices(invoices, 2);
-				this.setState({ invoicesToDisplay: lastestTwo });
-			} else {
-				const lastestThree = BillingUtils.getInvoices(invoices, 3);
-				this.setState({ invoicesToDisplay: lastestThree });
-			}
-		} catch (e) {}
+	renderRecentInvoices = invoices => {
+		const invoiceList = [];
+		for (let i = 0; i < NUM_INVOICES_TO_SHOW[this.props.breakpoint]; i++) {
+			const invoice = invoices[i];
+			invoiceList.push(
+				invoice ? (
+					<div key={invoice.invoiceId} className='recent-invoice'>
+						<div className='invoice-image image'>
+							{invoice.status && <img src={this.getIcon(invoice.status)} />}
+						</div>
+						<span className={`invoice-amount${invoice.status === 'Overdue' ? ' overdue' : ''}`}>
+							{invoice.invoiceAmount ? this.formatAmount(invoice.invoiceAmount) : ''}
+						</span>
+						<span className='invoice-number body10'>
+							{invoice.invoiceNumber
+								? `Invoice: #${invoice.invoiceNumber ? invoice.invoiceNumber : ''}`
+								: ''}
+						</span>
+						<span className='date-sent invoice-date body10'>
+							{invoice.billcycle
+								? `Date Sent: ${moment(invoice.billcycle).format('MM.DD.YY')}`
+								: ''}
+						</span>
+					</div>
+				) : (
+					<div className='recent-invoice empty' />
+				),
+			);
+		}
+
+		return invoiceList;
 	};
 
 	componentDidMount() {
 		this.getRecentInvoices();
 	}
-	componentDidUpdate(prevProps) {
-		const { breakpoint } = this.props;
-		if (prevProps.breakpoint !== breakpoint) {
-			this.getInvoicesToDisplay();
-		}
-	}
 
 	render() {
-		const { invoicesToDisplay } = this.state;
+		const { invoicesToDisplay, invoices } = this.state;
 		const { breakpoint } = this.props;
 		if (invoicesToDisplay) {
 			invoicesToDisplay.length === 1 ? invoicesToDisplay.push(EmptyInvoice) : '';
@@ -143,32 +120,9 @@ class RecentInvoices extends Component {
 
 		return (
 			<Fragment>
-				{invoicesToDisplay && (
+				{invoices && (
 					<div className={'recent-invoices-wrapper recent-invoices'}>
-						<div className='invoices invoice-list'>
-							{invoicesToDisplay.map(invoice => (
-								<div key={invoice.invoiceId} className='recent-invoice'>
-									<div className='invoice-image image'>
-										{invoice.status && <img src={this.getIcon(invoice.status)} />}
-									</div>
-									<span
-										className={`invoice-amount${invoice.status === 'Overdue' ? ' overdue' : ''}`}
-									>
-										{invoice.invoiceAmount ? this.formatAmount(invoice.invoiceAmount) : ''}
-									</span>
-									<span className='invoice-number body10'>
-										{invoice.invoiceNumber
-											? `Invoice: #${invoice.invoiceNumber ? invoice.invoiceNumber : ''}`
-											: ''}
-									</span>
-									<span className='date-sent invoice-date body10'>
-										{invoice.billcycle
-											? `Date Sent: ${moment(invoice.billcycle).format('MM.DD.YY')}`
-											: ''}
-									</span>
-								</div>
-							))}
-						</div>
+						<div className='invoices invoice-list'>{this.renderRecentInvoices(invoices)}</div>
 					</div>
 				)}
 			</Fragment>
