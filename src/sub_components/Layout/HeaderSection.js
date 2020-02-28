5df8afffbd7034df44a5275440950d3e8d6f54a7
Niamh <niamh.gilligan@dcblox.com>
2020-02-28 22:26:16 +0000

    [AW-1266]

--- src/sub_components/Layout/HeaderSection.js
+++ src/sub_components/Layout/HeaderSection.js
@@ -5,7 +5,7 @@ const HeaderSection = props => {
 	const grid = [{ i: 'card1', x: 0, y: 0, w: 10, h: 10, static: true }];
 
 	return (
-		<BloxGrid name='sample-header-grid' parentEl='.portal-header' contentGrid={grid}>
+		<BloxGrid name='header-grid' parentEl='.portal-header' contentGrid={grid}>
 			<div key='card1' className='sample-card-1'>
 				<LogoutComponent />
 			</div>
