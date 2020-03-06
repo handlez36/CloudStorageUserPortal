032e8a6d1f30b48e304d241e46431a75cc2163a7
Niamh <niamh.gilligan@dcblox.com>
2020-03-06 19:55:09 +0000

    [AW-1266]latest fonts added and stylesfor header component

--- src/sub_components/Layout/Header/AccountsComponent.js
+++ src/sub_components/Layout/Header/AccountsComponent.js
@@ -12,6 +12,9 @@ class AccountsComponent extends Component {
 		this.userProfileApi = new UserProfileApi();
 		this.state = {
 			currentModule: null,
+			currentSelectedCompany: '',
+			companyName: '',
+			role: '',
 		};
 	}
 
@@ -44,7 +47,7 @@ class AccountsComponent extends Component {
 				return (
 					<Fragment>
 						<div
-							className={`company-link${isSelectedCompany ? ' selected' : ''}`}
+							className={`company-link${isSelectedCompany ? ' selected header10 ' : ' header10'}`}
 							style={{ cursor: 'pointer' }}
 							onClick={() => this.companySwitch(company.organizationId)}
 						>
@@ -54,6 +57,44 @@ class AccountsComponent extends Component {
 					</Fragment>
 				);
 			});
+		}
+	};
+
+	checkNameLength = companyName => {
+		const { breakpoint } = this.props;
+		const currentName = document.querySelector('.company-name');
+		if (
+			currentName &&
+			currentName.innerHTML.length >= MAX_COMPANY_NAME_WIDTH[breakpoint] &&
+			!currentName.classList.contains('hovered')
+		) {
+			companyName = companyName.substring(0, MAX_COMPANY_NAME_WIDTH[breakpoint]) + '...';
+		}
+
+		this.setState({ companyName });
+	};
+	getCompanyName = () => {
+		const { company_info } = this.props;
+		let companyName = '';
+
+		if (company_info && company_info.customer && company_info.customer.companyName) {
+			companyName = company_info.customer.companyName;
+			this.getUsersRole(company_info.fuseBillId);
+		}
+
+		this.setState({ companyName }, () => {
+			this.checkNameLength(companyName);
+		});
+	};
+
+	componentDidMount() {
+		this.getCompanyName();
+	}
+	getUserName = user => {
+		const username = this.userProfileApi.getFirstAndLastName(user);
+
+		if (username.length >= 16) {
+			return username.slice(0, 16) + '...';
 		} else {
 			return (
 				<Fragment>
@@ -66,23 +107,43 @@ class AccountsComponent extends Component {
 		}
 	};
 
+	componentDidUpdate(prevProps) {
+		const { company_info } = this.props;
+
+		if (company_info !== prevProps.company_info) {
+			this.getCompanyName();
+		}
+	}
+	getUsersRole = id => {
+		const { memberships = {} } = this.props.auth_status;
+		const currentMembershipSelected = memberships.filter(
+			membership => Number(membership.organizationId) === Number(id),
+		);
+
+		this.setState({ role: currentMembershipSelected[0].role });
+	};
+
 	render() {
 		const { memberships = {} } = this.props.auth_status;
 		const { auth_status } = this.props;
+		const { companyName, role } = this.state;
+
 		return (
 			<div
 				className='accounts-component-wrapper'
 				onMouseOver={this.setExpandedClass}
 				onMouseOut={this.removeExpandedClass}
 			>
-				<div className='company-name body10'>{'Current cOMPA '}</div>
+				<div className='company-name header10'>{companyName}</div>
 				<div className='image'></div>
 				<div className='accounts-dropdown'>
 					<div className='single-user'>
-						<div className='title'>{'ADMINISTRATOR'}</div>
+						<div className='title header30'>{role}</div>
 						<div className='user-wrapper'>
-							<div className='user-name'>'NIamh gilligan'</div>
-							<div className='avatar'>'IMAGE'</div>
+							<div className='user-name header10'>{this.getUserName(auth_status)}</div>
+							<div className='avatar'>
+								<img src={this.avatarApi.getUserAvatar(auth_status)} />
+							</div>
 						</div>
 					</div>
 					<div className='multi-user'>{this.renderMultiUserList(memberships)}</div>
--- src/sub_components/Layout/Header/HeaderSection.js
+++ src/sub_components/Layout/Header/HeaderSection.js
@@ -2,19 +2,36 @@ import React from 'react';
 import BloxGrid from '../BloxMicroGrid';
 import LogoutComponent from './LogoutComponent';
 import AccountsComponent from './AccountsComponent';
+import BreadCrumbComponent from './BreadCrumbComponent';
 const HeaderSection = props => {
-	const grid = [
-		{ i: 'logout-component', x: 0, y: 0, w: 2, h: 8, static: true },
-		{ i: 'accounts-component', x: 135, y: 0, w: 2, h: 8, static: true },
-	];
+	const LAYOUT_CONFIG = {
+		[RESOLUTIONS.LOW]: {
+			logoutComponent: { x: 0, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			breadcrumbComponent: { x: 6, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			accountsComponent: { x: 12, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+		},
+		[RESOLUTIONS.MED]: {
+			logoutComponent: { x: 0, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			breadcrumbComponent: { x: 6, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			accountsComponent: { x: 12, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+		},
+		[RESOLUTIONS.HIGH]: {
+			logoutComponent: { x: 0, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			breadcrumbComponent: { x: 6, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+			accountsComponent: { x: 12, y: 0, dim: DIMENSIONS.TWO_BY_TWO },
+		},
+	};
 
 	return (
 		<BloxGrid name='header-grid' parentEl='.portal-header' contentGrid={grid}>
 			<div key='logout-component' className='logout-component'>
 				<LogoutComponent />
 			</div>
-			<div key='accounts-component' className='accounts-component'>
-				<AccountsComponent />
+			<div key='breadcrumbComponent'>
+				<BreadCrumbComponent />
+			</div>
+			<div key='accountsComponent' className='accounts-component'>
+				<AccountsComponent breakpoint={props.breakpoint} />
 			</div>
 		</BloxGrid>
 	);
