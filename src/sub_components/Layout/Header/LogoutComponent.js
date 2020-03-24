02def68fe7d2611a682552fefbabb4e210e94e03
Brandon McLean <brandonmclean@live.com>
2020-03-24 15:18:10 -0400

    [AW-1284] Reset user and path state variables after logging out

--- src/sub_components/Layout/Header/LogoutComponent.js
+++ src/sub_components/Layout/Header/LogoutComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+
 import { AvatarApi } from 'services/avatar';
 import { UserProfileApi } from 'services/userProfile';
 import { UserApi } from 'services/user';
 import BloxButton from 'sub_components/Common/BloxButton';
+import { logoutUser } from 'actions/auth';
+
 class LogoutComponent extends Component {
 	constructor(props) {
 		super(props);
@@ -18,7 +20,7 @@ class LogoutComponent extends Component {
 
 	getUserName = user => {
 		const username = this.userProfileApi.getFirstAndLastName(user);
-		//const username = 'ASDASDSDASDASDASDSADSADASDASDASDAS';
+
 		if (username.length >= 16) {
 			return username.slice(0, 16) + '...';
 		} else {
@@ -34,24 +36,17 @@ class LogoutComponent extends Component {
 		const header = document.querySelector('.name');
 		header.classList.remove('blue-animation');
 	};
+
 	logoutUser = () => {
+		const { logoutUser } = this.props;
 		const logoutButton = document.querySelector('.blox-button.logout');
+
 		if (logoutButton) {
 			logoutButton.classList.add(this.props.module);
 			logoutButton.classList.add('trigger-animation-swipe');
 		}
-		UserApi.logoutUser()
-			.then(response => {
-				const validResponse = response.status === 200 && response.data && !response.data.error;
-				if (validResponse) {
-					this.props.history.push('/login');
-				}
-			})
-			.catch(error => this.setState({ error }));
 
-		setTimeout(() => {
-			this.props.history.push('/login');
-		}, 3000);
+		logoutUser();
 	};
 
 	render() {
@@ -66,7 +61,6 @@ class LogoutComponent extends Component {
 					<img src={this.avatarApi.getUserAvatar(auth_status)} />
 				</div>
 				<div className='name header10'>{this.getUserName(auth_status)}</div>
-				{/* <div className='logout-dropdown'></div> */}
 				<div className='hover-wrapper'>
 					<div className='avatar'>
 						<img src={this.avatarApi.getUserAvatar(auth_status)} />
@@ -91,4 +85,7 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, null)(LogoutComponent);
+export default connect(
+	mapStateToProps,
+	{ logoutUser },
+)(LogoutComponent);
