efcdf198e5e164dadd9a750e165e8d7c4ce2e6d7
Niamh <niamh.gilligan@dcblox.com>
2020-03-26 14:02:17 +0000

    [AW-1293]

--- src/pages/Profile/UserMgmtPage.js
+++ src/pages/Profile/UserMgmtPage.js
@@ -9,13 +9,13 @@ import { RESOLUTIONS, BREAKPOINT_COLCOUNT_MAP } from 'services/config';
 
 const LAYOUT_CONFIG = {
 	[RESOLUTIONS.LOW]: {
-		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX, customHeight: 85 },
+		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX },
 	},
 	[RESOLUTIONS.MED]: {
-		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX, customHeight: 112 },
+		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX },
 	},
 	[RESOLUTIONS.HIGH]: {
-		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX, customHeight: 160 },
+		userManagement: { x: 4, y: 0, dim: DIMENSIONS.TWO_BY_SIX },
 	},
 };
 
